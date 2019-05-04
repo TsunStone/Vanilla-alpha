5317f9c27aa6f289b815886e40a8bf45fadd05f1
Amoo0001 <50158189+Amoo0001@users.noreply.github.com>
2019-05-03 19:48:33 -0500

    Update Mod.lua

--- Mods/Items/Dirt/Mod.lua
+++ Mods/Items/Dirt/Mod.lua
@@ -4,27 +4,3 @@ addCommonBlocks{
 	xOf4096   = 15*256,
 	yOf4096   = 0,
 }
-
---[[
-{
-	"name": "Dirt Items",
-	"type": "ItemSet",
-	"itemset_min": 256,
-	"itemset_max": 511,
-	"items": [
-		{
-			"index": 256,
-			"icon_x": 0,
-			"icon_y": 0,
-			"icon_width": 32,
-			"icon_height": 32,
-			"name": "Dirt",
-			"type": "Ore",
-			"subtype": "Unrefined",
-			"material": "Dirt",
-			"buy": 0,
-			"sell": 0
-		}
-	]
-}
-]]
