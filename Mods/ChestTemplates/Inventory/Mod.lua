0dd21197232035784e1c21d9d89151a994433bc3
Amoo0001 <50158189+Amoo0001@users.noreply.github.com>
2019-05-18 13:53:10 -0700

    Update Mod.lua

--- Mods/ChestTemplates/Inventory/Mod.lua
+++ Mods/ChestTemplates/Inventory/Mod.lua
@@ -1,3 +1,11 @@
+addChestTemplate{
+	name          = "Inventory",
+	width         = 9,
+	height        = 4,
+	playerPrivate = true,
+	portable      = true,
+}
+
 implement("ToggleInventory", function()
 	alert("Hi from Lua!")
 end)
