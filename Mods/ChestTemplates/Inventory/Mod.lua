4bda0d07c8a777e0aabd9277cb432700008ceaee
Amoo0001 <50158189+Amoo0001@users.noreply.github.com>
2019-05-18 16:08:21 -0700

    Update Mod.lua

--- Mods/ChestTemplates/Inventory/Mod.lua
+++ Mods/ChestTemplates/Inventory/Mod.lua
@@ -14,7 +14,14 @@ implement("ToggleInventory", function()
 	TsunStone_Vanilla_ChestTemplates_Inventory.isOpen = isNowOpen
 	if (isNowOpen) then
 		alert("Opening inventory")
+		TsunStone_Vanilla_ChestTemplates_Inventory.panel = newPanel{
+			xAnchor = 0.5
+			yAnchor = 0.5
+			width   = 0.75 * 16/9
+			height  = 0.75
+		}
 	else
 		alert("Closing inventory")
+		TsunStone_Vanilla_ChestTemplates_Inventory.panel.destroy()
 	end
 end)
