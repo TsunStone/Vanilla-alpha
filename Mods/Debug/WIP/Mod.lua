306e4bb95308370bc3a37c2d28bc5747a740c30f
Amoo0001 <50158189+Amoo0001@users.noreply.github.com>
2019-05-21 21:00:30 -0700

    Update Mod.lua

--- Mods/Debug/WIP/Mod.lua
+++ Mods/Debug/WIP/Mod.lua
@@ -1,3 +1,4 @@
 hook("ToggleInventory", function()
-	chat("System", "Hi there, [b]bold test[/b], [i]italic test[/i], [u]underline test[/u].")
+	chat("System", "Hi there, [b]bold test[/b], [i]italic test[/i], [u]underline test[/u], [s]strikethrough test[/s].")
+	chat("System", ">Greentext test")
 end)
